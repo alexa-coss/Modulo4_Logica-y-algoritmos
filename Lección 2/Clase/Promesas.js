0d340d8436a15165e4b94e8e47fba8758c9bea3f
Ale Coss <alexacoss99@gmail.com>
2025-04-08 21:34:09 -0600

    Actualización Clase 2

--- Lección 2/Clase/Promesas.js
+++ Lección 2/Clase/Promesas.js
@@ -44,3 +44,22 @@ function paraElForEach(elemento) {
 const frutas = ["Manzana", "Platano", "Sandía", "Pera"];
 
 frutas.forEach(paraElForEach);
+
+
+/* ¿Que pasa si necesito el valor que estoy esperando para poder continuar? */
+
+// Usando async/await
+async function ejecutarConsulta() {
+    try {
+        /* Esto obliga a "esperar" el resultado de la promesa */
+        const resultado = await obtenerDestino("Londres");
+        notificarUsuario(resultado);
+    } catch (error) {
+        manejarError(error);
+    } finally {
+        console.log("Siempre me ejecuto");
+    }
+}
+
+// Ejecutar la función asíncrona
+ejecutarConsulta();
